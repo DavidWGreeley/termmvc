83bcdfe00ec3bc839ddbab0958303f1ecae7ab47
David <David@Davids-PC>
2016-04-13 00:30:35 -0230

    Stage 2 Commit

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -9,17 +9,8 @@ App::uses('AppController', 'Controller');
  * @property SessionComponent $Session
  */
 class UsersController extends AppController {
-    public function beforeFilter() {
-        parent::beforeFilter();
-        }
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator', 'Flash', 'Session');
-
-        public function login() {
+    
+    public function login() {
             if($this->request->is('post')) {
                 if($this->Auth->login()) {
                     return $this->redirect($this->Auth->redirectUrl());
@@ -28,9 +19,31 @@ class UsersController extends AppController {
                 }
             }
         }
+        public function admin_login() {
+            if($this->request->is('post')) {
+                if($this->Auth->login()) {
+                    return $this->redirect($this->Auth->redirectUrl());
+                } else {
+                    $this->Session->setFlash('Invalid Username or Password');
+                }
+            }
+	}
+        
         public function logout() {
             return $this->redirect ($this->Auth->logout());
         }
+        public function admin_logout() {
+            return $this->redirect ($this->Auth->logout());
+        }
+    
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator', 'Flash', 'Session');
+
 /**
  * index method
  *
@@ -64,10 +77,7 @@ class UsersController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->User->create();
-                        $this->request->data['User']['password'] = AuthComponent::password($this->request->data['User']['password']);
-			$this->request->data['User']['role'] = 1;
-                                
-                        if ($this->User->save($this->request->data)) {
+			if ($this->User->save($this->request->data)) {
 				$this->Flash->success(__('The user has been saved.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
@@ -108,6 +118,93 @@ class UsersController extends AppController {
  * @return void
  */
 	public function delete($id = null) {
+		$this->User->id = $id;
+		if (!$this->User->exists()) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->User->delete()) {
+			$this->Flash->success(__('The user has been deleted.'));
+		} else {
+			$this->Flash->error(__('The user could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+
+/**
+ * admin_index method
+ *
+ * @return void
+ */
+	public function admin_index() {
+		$this->User->recursive = 0;
+		$this->set('users', $this->Paginator->paginate());
+	}
+
+/**
+ * admin_view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_view($id = null) {
+		if (!$this->User->exists($id)) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+		$this->set('user', $this->User->find('first', $options));
+	}
+
+/**
+ * admin_add method
+ *
+ * @return void
+ */
+	public function admin_add() {
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->Flash->success(__('The user has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__('The user could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * admin_edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_edit($id = null) {
+		if (!$this->User->exists($id)) {
+			throw new NotFoundException(__('Invalid user'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->User->save($this->request->data)) {
+				$this->Flash->success(__('The user has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Flash->error(__('The user could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+			$this->request->data = $this->User->find('first', $options);
+		}
+	}
+
+/**
+ * admin_delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function admin_delete($id = null) {
 		$this->User->id = $id;
 		if (!$this->User->exists()) {
 			throw new NotFoundException(__('Invalid user'));
